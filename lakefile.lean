af441680bfbb819c790482790c2b383592fd402f
David E. Narváez <david.narvaez@computer.org>
2023-11-25 20:37:53 -0500

    Fixing the Dependency Versions
    The new dependency brought in all sorts of chaos, including the fact
    that many things do not build with Lean 4.3.0, so for now we have to
    stick to Lean 4.2.0.

--- lakefile.lean
+++ lakefile.lean
@@ -5,10 +5,9 @@ package «formal_ramsey» {
   -- add any package configuration options here
 }
 
-require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
+require mathlib from git "https://github.com/leanprover-community/mathlib4.git"@"v4.2.0"
 
-require proofwidgets from git "https://github.com/EdAyers/ProofWidgets4"@"v0.0.23"
+require proofwidgets from git "https://github.com/EdAyers/ProofWidgets4"@"v0.0.22"
 
 @[default_target]
 lean_lib «FormalRamsey» {
