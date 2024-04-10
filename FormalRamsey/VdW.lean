c35980915c876ad3b2b8ae190fbbd8dc49be6893
David E. Narváez <david.narvaez@computer.org>
2024-04-10 18:57:45 -0400

    Removing Commented Code to Generate the vdW9 CNF Formula
    Since we now have the encoder elsewhere, it is not necessary to keep
    this around.

--- FormalRamsey/VdW.lean
+++ FormalRamsey/VdW.lean
@@ -420,8 +420,6 @@ instance existsIsArithProgDec {N : ℕ} : ∀ (l : List (Fin N.succ)), Decidable
           simp [isArithProg]
           tauto
 
--- #eval let allVars := List.filter (fun l' => decide (∃ d, isArithProg l' d)) (List.sublistsLen 3 (List.finRange (Nat.succ 8))); "p cnf " ++ (reprStr (List.finRange (Nat.succ 8)).length) ++ " " ++ (reprStr (2 * allVars.length)) ++ " " ++ " 0 ".intercalate (List.map (fun c => " ".intercalate (c.map (λ l => reprStr (l.val + 1)))) allVars) ++ " 0 " ++ " 0 ".intercalate (List.map (fun c => " ".intercalate (c.map (λ l => reprStr (Int.negSucc l.val)))) allVars) ++ " 0"
-
 lrat_proof vdW9
   "p cnf 9 32 7 8 9 0 6 7 8 0 5 7 9 0 5 6 7 0 4 6 8 0 4 5 6 0 3 6 9 0 3 5 7 0 3 4 5 0 2 5 8 0 2 4 6 0 2 3 4 0 1 5 9 0 1 4 7 0 1 3 5 0 1 2 3 0 -7 -8 -9 0 -6 -7 -8 0 -5 -7 -9 0 -5 -6 -7 0 -4 -6 -8 0 -4 -5 -6 0 -3 -6 -9 0 -3 -5 -7 0 -3 -4 -5 0 -2 -5 -8 0 -2 -4 -6 0 -2 -3 -4 0 -1 -5 -9 0 -1 -4 -7 0 -1 -3 -5 0 -1 -2 -3 0"
   "33 -6 -8 -9 0 17 21 23 9 14 29 0 34 -8 -9 0 17 33 4 29 26 14 16 25 0 34 d 17 0 35 -8 -9 0 34 0 35 d 33 0 36 7 -9 0 35 2 23 8 22 29 14 0 37 -6 -9 0 36 19 35 10 23 27 9 0 38 -9 0 35 36 19 10 37 6 30 28 15 0 39 5 6 0 38 3 13 6 30 0 40 -5 -3 0 24 31 25 14 0 40 d 24 0 41 6 0 38 7 39 40 0 42 -5 0 41 38 20 22 40 1 12 26 0 43 7 0 42 38 3 0 44 1 0 42 38 13 0 45 -8 0 43 41 18 0 46 -4 0 44 43 30 0 47 2 0 45 42 10 0 48 3 0 46 42 9 0 49 0 47 48 44 32 0"
