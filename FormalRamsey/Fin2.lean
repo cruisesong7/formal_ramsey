eb78f7ebd6b97dc12c482cb0e879d36d32907650
David E. Narváez <david.narvaez@computer.org>
2025-03-16 17:09:49 -0400

    Minor fixes in Fin2.lean

--- FormalRamsey/Fin2.lean
+++ FormalRamsey/Fin2.lean
@@ -7,7 +7,6 @@ theorem univ_fin2 : (Finset.univ : Finset (Fin 2)) = {0, 1} := by
   fin_cases x <;> simp
 
 lemma notc : ∀ {c x y : Fin 2}, x ≠ c → y ≠ c → x = y := by
-
   intros c x y _ _
   fin_cases c 
   all_goals{
@@ -18,7 +17,6 @@ lemma notc : ∀ {c x y : Fin 2}, x ≠ c → y ≠ c → x = y := by
       tauto
     }
   }
-  done
 
 lemma not0_eq1: ∀ {x: Fin 2}, x ≠ 0 ↔ x = 1 := by
   intro 
@@ -28,4 +26,3 @@ lemma not0_eq1: ∀ {x: Fin 2}, x ≠ 0 ↔ x = 1 := by
     apply notc xneq0 _1_neq0
   · intro
     simp_all
-  done
